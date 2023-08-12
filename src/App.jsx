70a252494520a5e537350923131dd3e5f11bd155
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-08-12 19:22:21 +0700

    setup react router and architect the main folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-function App() {
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import PricingPage from './pages/PricingPage';
+import ContactPage from './pages/ContactPage';
+import SpacesPage from './pages/SpacesPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
+function App() {
   return (
-    <div className="flex items-center justify-center">
-      <div>Something</div>
+    <div className="flex flex-col items-center justify-center">
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/about"
+          element={<AboutPage />}
+        />
+        <Route
+          path="/pricing"
+          element={<PricingPage />}
+        />
+        <Route
+          path="/contact"
+          element={<ContactPage />}
+        />
+        <Route
+          path="/spaces"
+          element={<SpacesPage />}
+        />
+      </Routes>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/SpacesPage.jsx
@@ -0,0 +1,5 @@
+export default function SpacesPage() {
+  return (
+    <div>SpacesPage</div>
+  )
+}
