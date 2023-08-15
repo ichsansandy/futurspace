807d70432889a29062d842408ad282d293484cfe
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-08-15 18:06:10 +0700

    finish contact page

--- src/components/Contact/ContactClient.jsx
+++ src/components/Contact/ContactClient.jsx
@@ -10,29 +10,29 @@ export default function ContactClient() {
       <p className=" sm:text-[26px] sm:leading-[32px] text-center max-w-[450px] web:max-w-none font-bold">
         Trusted by top companies and startups around the world
       </p>
-      <div className="grayscale flex justify-center items-center flex-wrap box-border gap-3">
+      <div className="grayscale flex justify-center items-center flex-wrap box-border gap-3 sm:gap-5 web:gap-20">
         <img
-          className="w-[15%]"
+          className="w-[15%] web:w-[10%]"
           alt="google"
           src={google}
         />
         <img
-          className="w-[15%]"
+          className="w-[15%] web:w-[10%]"
           alt="creativeMarket"
           src={creativeMarket}
         />
         <img
-          className="w-[15%]"
+          className="w-[15%] web:w-[10%]"
           alt="airbnb"
           src={airbnb}
         />
         <img
-          className="w-[15%]"
+          className="w-[15%] web:w-[10%]"
           alt="shopify"
           src={shopify}
         />
         <img
-          className="w-[15%] mt-3"
+          className="w-[15%] web:w-[10%] mt-3"
           alt="amazon"
           src={amazon}
         />
